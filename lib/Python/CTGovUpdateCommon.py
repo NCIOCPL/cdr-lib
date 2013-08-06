67a81cb5e373f32e6a0c714331ffd4e41f9bf10f
Volker Englisch <venglisch@users.noreply.github.com>
2013-08-06 21:08:57 +0000

    <R11931> Fixing the URL to display the host's CNAME (OCECDR-3626).

--- lib/Python/CTGovUpdateCommon.py
+++ lib/Python/CTGovUpdateCommon.py
@@ -8,8 +8,10 @@
 import cdr
 
 JOB_NAME    = "Imported CTGovProtocols vs. CWDs"
-REPORT_BASE = cdr.BASE_DIR + "/reports/"
+REPORT_BASE = "D:/cdr/reports/"
 REPORT_FILE = "CTGov-CWD-diffs.html"
 SCRIPT      = "CTGovUpdateReportBatch.py"
-REPORT_URL  = cdr.getHostName()[2] + "/cdrreports/%s" % REPORT_FILE
+REPORT_URL  = "https://%s.%s/cdrreports/%s" % (cdr.h.host['APPC'][0], 
+                                               cdr.h.host['APPC'][1],
+                                               REPORT_FILE)
 LF          = cdr.DEFAULT_LOGFILE
